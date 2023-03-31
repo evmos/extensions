3fc3f2104461e9abf5041007054381b421feb369
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2023-03-31 15:09:21 +0300

    imp: Improve folder structure and add Bech32, Types and ICS20

---
+++ precompiles/common/Types.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: LGPL-v3
+pragma solidity >=0.8.17;
+
+struct Dec {
+    uint256 value;
+    uint8 precision;
+}
+
+/// @dev Coin is a struct that represents a token with a denomination and an amount.
+struct Coin {
+    string denom;
+    uint256 amount;
+}
+
+/// @dev DecCoin is a struct that represents a token with a denomination, an amount and a precision.
+struct DecCoin {
+    string denom;
+    uint256 amount;
+    uint8 precision;
+}
+
+/// @dev PageResponse is a struct that represents a page response.
+struct PageResponse {
+    bytes nextKey;
+    uint64 total;
+}
