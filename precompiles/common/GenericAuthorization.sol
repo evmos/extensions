241ba0f9828895624b85f66d1479c323c5fe6eef
tom <tomasguerraalda@hotmail.com>
2023-05-05 15:57:41 -0300

    chore: update generic auth interface

--- precompiles/common/GenericAuthorization.sol
+++ precompiles/common/GenericAuthorization.sol
@@ -14,6 +14,15 @@ interface GenericAuthorizationI {
         string[] calldata methods
     ) external returns (bool approved);
 
+    /// @dev Revokes a list of Cosmos transactions.
+    /// @param spender The address which will spend the funds.
+    /// @param methods The message type URLs of the methods to revoke.
+    /// @return revoked Boolean value to indicate if the revocation was successful.
+    function revoke(
+        address spender,
+        string[] calldata methods
+    ) external returns (bool revoked);
+
     /// @dev This event is emitted when the allowance of a spender is set by a call to the approve method.
     /// The value field specifies the new allowance and the methods field holds the information for which methods
     /// the approval was set.
@@ -21,6 +30,16 @@ interface GenericAuthorizationI {
     /// @param spender The address which will spend the funds.
     /// @param methods The message type URLs of the methods for which the approval is set.
     event Approval(
+        address indexed owner,
+        address indexed spender,
+        string[] methods
+    );
+
+    /// @dev This event is emitted when an owner revokes a spender's allowance.
+    /// @param owner The owner of the tokens.
+    /// @param spender The address which will spend the funds.
+    /// @param methods The message type URLs of the methods for which the approval is set.
+    event Revocation(
         address indexed owner,
         address indexed spender,
         string[] methods
