8571662bf321984c15794da7087c61e0f3403cb7
Tom <54514587+GAtom22@users.noreply.github.com>
2023-06-07 04:26:29 -0300

    chore: update IBC transfer extension (#11)

--- precompiles/common/Types.sol
+++ precompiles/common/Types.sol
@@ -19,8 +19,34 @@ struct DecCoin {
     uint8 precision;
 }
 
+/// @dev PageRequest is a struct that represents a page request.
+struct PageRequest {
+    bytes key;
+    uint64 offset;
+    uint64 limit;
+    bool countTotal;
+    bool reverse;
+}
+
 /// @dev PageResponse is a struct that represents a page response.
 struct PageResponse {
     bytes nextKey;
     uint64 total;
+}
+
+/// @dev Height is a monotonically increasing data type
+/// that can be compared against another Height for the purposes of updating and
+/// freezing clients
+///
+/// Normally the RevisionHeight is incremented at each height while keeping
+/// RevisionNumber the same. However some consensus algorithms may choose to
+/// reset the height in certain conditions e.g. hard forks, state-machine
+/// breaking changes In these cases, the RevisionNumber is incremented so that
+/// height continues to be monotonically increasing even as the RevisionHeight
+/// gets reset
+struct Height {
+    // the revision that the client is currently on
+    uint64 revisionNumber;
+    // the height within the given revision
+    uint64 revisionHeight;
 }
--- precompiles/stateful/IBCTransfer.sol
+++ precompiles/stateful/IBCTransfer.sol
@@ -1,41 +1,37 @@
 // SPDX-License-Identifier: LGPL-v3
 pragma solidity >=0.8.17;
 
+import "../common/Types.sol";
+
 /// @dev The ICS20I contract's address.
 address constant IBC_TRANSFER_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000000802;
 
 /// @dev The ICS20 contract's instance.
-IBCTransferI constant IBC_TRANSFER_CONTRACT = IBCTransferI(ICS20_PRECOMPILE_ADDRESS);
+IBCTransferI constant IBC_TRANSFER_CONTRACT = IBCTransferI(
+    IBC_TRANSFER_PRECOMPILE_ADDRESS
+);
 
-// Height is a monotonically increasing data type
-// that can be compared against another Height for the purposes of updating and
-// freezing clients
-//
-// Normally the RevisionHeight is incremented at each height while keeping
-// RevisionNumber the same. However some consensus algorithms may choose to
-// reset the height in certain conditions e.g. hard forks, state-machine
-// breaking changes In these cases, the RevisionNumber is incremented so that
-// height continues to be monotonically increasing even as the RevisionHeight
-// gets reset
-struct Height {
-  // the revision that the client is currently on
-  uint64 revisionNumber;
-  // the height within the given revision
-  uint64 revisionHeight;
+/// @dev DenomTrace contains the base denomination for ICS20 fungible tokens and the
+/// source tracing information path.
+struct DenomTrace {
+    // path defines the chain of port/channel identifiers used for tracing the
+    // source of the fungible token.
+    string path;
+    // base denomination of the relayed fungible token.
+    string baseDenom;
 }
 
-// DenomTrace contains the base denomination for ICS20 fungible tokens and the
-// source tracing information path.
-struct DenomTrace {
-  // path defines the chain of port/channel identifiers used for tracing the
-	// source of the fungible token.
-  string path;
-  // base denomination of the relayed fungible token.
-  string baseDenom;
+/// @dev Allocation defines the spend limit for a particular IBC port and channel
+struct Allocation {
+    string sourcePort;
+    string sourceChannel;
+    string denom;
+    uint256 amount;
+    string[] allowList;
 }
 
 /// @author Evmos Team
-/// @title ICS20 Transfer Precompiled Contract
+/// @title IBC Transfer Precompiled Contract
 /// @dev The interface through which solidity contracts will interact with IBC Transfer (ICS20)
 /// FIXME: update address
 /// @custom:address 0x0000000000000000000000000000000000000802
@@ -51,38 +47,41 @@ interface IBCTransferI {
     /// @param timeoutTimestamp the bech32 address of the receiver
     /// @param memo the bech32 address of the receiver
     function transfer(
-      string memory sourcePort,
-      string memory sourceChannel,
-      string memory denom,
-      uint256 amount,
-      address sender,
-      string memory receiver,
-      Height memory timeoutHeight,
-      uint64 timeoutTimestamp,
-      string memory memo
+        string memory sourcePort,
+        string memory sourceChannel,
+        string memory denom,
+        uint256 amount,
+        address sender,
+        string memory receiver,
+        Height memory timeoutHeight,
+        uint64 timeoutTimestamp,
+        string memory memo
     ) external returns (uint64 nextSequence);
 
     /// @dev DenomTraces defines a method for returning all denom traces.
-    function denomTraces(
-    ) external returns (DenomTrace[] memory denomTraces);
+    function denomTraces() external returns (DenomTrace[] memory denomTraces);
 
     /// @dev DenomTrace defines a method for returning a denom trace.
     function denomTrace(
-      string memory hash
+        string memory hash
     ) external returns (DenomTrace memory denomTrace);
 
     /// @dev DenomHash defines a method for returning a hash of the denomination trace info.
     function denomHash(
-      string memory trace
+        string memory trace
     ) external returns (string memory hash);
 
     /// @dev Approves IBC transfer with a specific amount of tokens.
     /// @param spender spender The address which will spend the funds.
-    /// @param amount The amount of tokens to be spent.
+    /// @param allocations the allocations for the authorization.
     function approve(
         address spender,
-        uint256 amount
+        Allocation[] calldata allocations
     ) external returns (bool approved);
+
+    /// @dev Revokes IBC transfer authorization for a specific spender
+    /// @param spender spender The address which will spend the funds.
+    function revoke(address spender) external returns (bool revoked);
 
     /// @dev Returns the remaining number of tokens that spender will be allowed to spend on behalf of owner through
     /// IBC transfers. This is zero by default.
@@ -93,42 +92,83 @@ interface IBCTransferI {
         address spender
     ) external view returns (uint256 remaining);
 
-    /// @dev Increase the allowance of a given spender by a specific amount of tokens for IBC transfer methods.
+    /// @dev Increase the allowance of a given spender by a specific amount of tokens and IBC connection for IBC transfer methods.
     /// @param spender The address which will spend the funds.
+    /// @param sourcePort The source port of the IBC transaction.
+    /// @param sourceChannel The source channel of the IBC transaction.
+    /// @param denom The denomination of the tokens transferred.
     /// @param amount The amount of tokens to be spent.
     function increaseAllowance(
         address spender,
+        string calldata sourcePort,
+        string calldata sourceChannel,
+        string calldata denom,
         uint256 amount
     ) external returns (bool approved);
 
     /// @dev Decreases the allowance of a given spender by a specific amount of tokens for for IBC transfer methods.
     /// @param spender The address which will spend the funds.
+    /// @param sourcePort The source port of the IBC transaction.
+    /// @param sourceChannel The source channel of the IBC transaction.
+    /// @param denom The denomination of the tokens transferred.
     /// @param amount The amount of tokens to be spent.
     function decreaseAllowance(
         address spender,
+        string calldata sourcePort,
+        string calldata sourceChannel,
+        string calldata denom,
         uint256 amount
     ) external returns (bool approved);
 
-
-    /// @dev Emitted when a transfer is executed.
+    /// @dev Emitted when an ICS-20 transfer is executed.
     /// @param sender The address of the sender.
     /// @param receiver The address of the receiver.
     /// @param denom The denomination of the tokens transferred.
     /// @param amount The amount of tokens transferred.
+    /// @param memo The IBC transaction memo.
     event IBCTransfer(
         address indexed sender,
-        address indexed receiver,
+        string indexed receiver,
+        string denom,
+        uint256 amount,
+        string memo
+    );
+
+    /// @dev Emitted when an ICS-20 transfer authorization is granted.
+    /// @param grantee The address of the grantee.
+    /// @param granter The address of the granter.
+    /// @param sourcePort The source port of the IBC transaction.
+    /// @param sourceChannel The source channel of the IBC transaction.
+    /// @param denom The denomination of the tokens transferred.
+    /// @param amount The amount of tokens transferred.
+    event IBCTransferAuthorization(
+        address indexed grantee,
+        address indexed granter,
+        string sourcePort,
+        string sourceChannel,
         string denom,
         uint256 amount
     );
 
-    /// @dev Emitted when an approval is executed.
-    /// @param owner The address of the owner.
-    /// @param spender The address of the spender.
-    /// @param value The amount of tokens approved.
-    event Approval(
+    /// @dev This event is emitted when an owner revokes a spender's allowance.
+    /// @param owner The owner of the tokens.
+    /// @param spender The address which will spend the funds.
+    event RevokeIBCTransferAuthorization(
+        address indexed owner,
+        address indexed spender
+    );
+
+    /// @dev This event is emitted when the allowance of a spender is changed by a call to the decrease or increase
+    /// allowance method. The values field specifies the new allowances and the methods field holds the
+    /// information for which methods the approval was set.
+    /// @param owner The owner of the tokens.
+    /// @param spender The address which will spend the funds.
+    /// @param methods The message type URLs of the methods for which the approval is set.
+    /// @param values The amounts of tokens approved to be spent.
+    event AllowanceChange(
         address indexed owner,
         address indexed spender,
-        uint256 value
+        string[] methods,
+        uint256[] values
     );
 }
