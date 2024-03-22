b988f8a616cc5c4a348cb15ebeb2cb67930929bb
Vlad <vladislav.varadinov@gmail.com>
2024-03-22 12:38:40 +0200

    fix: update imports

--- outposts/osmosis/XCSOutpost.sol
+++ outposts/osmosis/XCSOutpost.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.17;
 
-import "ICS20.sol";
+import ".../../../precompiles/stateful/ICS20.sol";
+import "../../precompiles/common/Types.sol";
 
 contract XCSOutpost {
 
